5ad1193dac9c3bf2927e0c5fd63bb13fbd64a060
karamBarakat <karam.barakat.99@gmail.com>
2021-08-16 09:58:57 +0800

    prepare for fortfolio

---
+++ index.php
@@ -0,0 +1,435 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Presentation</title>
+
+    <link rel="shortcut icon" href="invert-logo (3).png" type="image/x-icon" />
+    <!-- stylesheets -->
+    <link rel="stylesheet" href="./css/bootstrap/bootstrap.css" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <script
+      src="https://kit.fontawesome.com/937d72c3e9.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body data-spy="scroll" data-target="#navbar-spy">
+    <header
+      id="navbar-spy"
+      class="mt-4 container container-nav position-absolute fixed-top"
+    >
+      <header class="px-3 navbar navbar-expand-sm navbar-dark bg-trasparent">
+        <div class="navbar-brand">
+          <img src="./invert-logo.png" alt="logo" />
+        </div>
+        <span
+          class="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#header-colps"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </span>
+        <div id="header-colps" class="navbar-collapse collapse">
+          <ul class="navbar-nav ml-auto nav-pills">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="#">ABOUT US</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">SERVICES</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">CONTACT</a></li>
+          </ul>
+        </div>
+      </header>
+    </header>
+    <section id="intro">
+      <div
+        id="intro-background"
+        class="backgroundImage"
+        style="background-image: url('./img/slider-02.jpg')"
+      >
+        <div class="overlay">
+          <div
+            id="intro-con"
+            class="
+              container
+              d-flex
+              flex-column
+              align-items-center
+              justify-content-center
+            "
+          >
+            <h1 class="mb-2 display-4 text-light">THINK IN MOTION</h1>
+            <p class="mb-5 text-light">WE CREATE. WE DEVELOP. WE INSPIRE.</p>
+            <div class="d-flex">
+              <botton class="btn btn-lg btn-outline-light">Who We Are</botton>
+              <botton class="btn btn-lg btn-outline-light ml-3"
+                >Get In Touch</botton
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="container my-5">
+      <p class="lead text-center mb-4">WELCOME TO OUR COMPANY</p>
+      <div class="row">
+        <p class="col lead">
+          TO MEET TODAY'S CHALLENGES, WE'VE CREATED A UNIQUE, MORE PERSONAL
+          APPROACH TO INVESTING.
+        </p>
+        <p class="col lead">
+          With a community of over 400 million users (and a majority age group
+          being 18 to 29), wouldn’t you want to make sure you’re monitoring what
+          people are posting on social networks? Especially if the demographic
+          of your audience falls in that age range.
+        </p>
+      </div>
+    </section>
+
+    <section id="section3" class="container">
+      <div class="row middle-cards">
+        <div class="col p-5">
+          <p>WHAT WE DO</p>
+          <h1>SERVICES</h1>
+        </div>
+        <div class="col p-5">
+          <p>WHAT WE DO</p>
+          <h1>SERVICES</h1>
+        </div>
+        <div class="col p-5">
+          <p>WHAT WE DO</p>
+          <h1>SERVICES</h1>
+        </div>
+      </div>
+    </section>
+
+    <section id="section4" class="py-5 bg-dark text-light">
+      <div class="container">
+        <div class="row my-5">
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-clock mb-2"></i>
+            <span class="mt-1 mb-3 text-thick">2020</span>
+            <span>ESTABLISHED</span>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-project-diagram mb-2"></i>
+            <span class="mt-1 mb-3 text-thick">4263</span>
+            <span>PROJECTS DONE</span>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-user-tie mb-2"></i>
+            <span class="mt-1 mb-3 text-thick">+20</span>
+            <span>BUSINESS PARTHNERS</span>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-user-plus mb-2"></i>
+            <span class="mt-1 mb-3 text-thick">+600</span>
+            <span>HAPPY CLIENTS</span>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-briefcase mb-2"></i>
+            <span class="mt-1 mb-3 text-thick">+10</span>
+            <span>staff</span>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="section5">
+      <div
+        id="section5-background"
+        style="background-image: url('./img/slider-01.jpg')"
+      >
+        <div class="section5-overlay">
+          <div
+            id="section5-con"
+            class="
+              container
+              d-flex
+              flex-column
+              align-items-center
+              justify-content-center
+            "
+          >
+            <h1 class="mb-2 display-4 text-light">
+              AMAZING VIDEOS & PRESENTATIONS
+            </h1>
+            <p class="mb-5 text-light">WE CREATE. WE DEVELOP. WE INSPIRE.</p>
+            <div class="d-flex">
+              <botton class="btn btn-lg btn-outline-light">Who We Are</botton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="section6" class="pt-2 pb-5 bg-light">
+      <div class="container">
+        <div class="row my-5">
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-clock mb-2 mt-5"></i>
+            <span class="mt-1 mb-3 text-thick">2020</span>
+            <span>ESTABLISHED</span>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-project-diagram mb-2 mt-5"></i>
+            <span class="mt-1 mb-3 text-thick">4263</span>
+            <span>PROJECTS DONE</span>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-user-tie mb-2 mt-5"></i>
+            <span class="mt-1 mb-3 text-thick">+20</span>
+            <span>BUSINESS PARTHNERS</span>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-user-plus mb-2 mt-5"></i>
+            <span class="mt-1 mb-3 text-thick">+600</span>
+            <span>HAPPY CLIENTS</span>
+          </div>
+          <div class="col-12"></div>
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-briefcase mb-2 mt-5"></i>
+            <span class="mt-1 mb-3 text-thick">+10</span>
+            <span>staff</span>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-briefcase mb-2 mt-5"></i>
+            <span class="mt-1 mb-3 text-thick">+10</span>
+            <span>staff</span>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-briefcase mb-2 mt-5"></i>
+            <span class="mt-1 mb-3 text-thick">+10</span>
+            <span>staff</span>
+          </div>
+          <div class="col d-flex flex-column align-items-center">
+            <i class="fas fa-2x fa-briefcase mb-2 mt-5"></i>
+            <span class="mt-1 mb-3 text-thick">+10</span>
+            <span>staff</span>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <p class="lead my-5 text-center">OUR LATEST WORKS</p>
+      <div class="row no-gutters">
+        <div class="col-6 col-md-3">
+          <div class="jet-container">
+            <div class="jet-back">
+              <img
+                class="img-fluid"
+                src="./img/jet/pexels-photo-2896358.jpeg"
+                alt=""
+              />
+            </div>
+
+            <div
+              class="
+                overlay
+                jet-front
+                d-flex
+                flex-column
+                align-items-center
+                justify-content-center
+                text-white
+              "
+            >
+              <p class="lead">STRATEGY</p>
+              <h5>GETTING STARTED</h5>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-6 col-md-3">
+          <div class="jet-container">
+            <div class="jet-back">
+              <img
+                class="img-fluid"
+                src="./img/jet/pexels-photo-2699877.jpeg"
+                alt=""
+              />
+            </div>
+
+            <div
+              class="
+                overlay
+                jet-front
+                d-flex
+                flex-column
+                align-items-center
+                justify-content-center
+                text-white
+              "
+            >
+              <p class="lead">STRATEGY</p>
+              <h5>GETTING STARTED</h5>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-6 col-md-3">
+          <div class="jet-container">
+            <div class="jet-back">
+              <img
+                class="img-fluid"
+                src="./img/jet/pexels-photo-3636489.jpeg"
+                alt=""
+              />
+            </div>
+
+            <div
+              class="
+                overlay
+                jet-front
+                d-flex
+                flex-column
+                align-items-center
+                justify-content-center
+                text-white
+              "
+            >
+              <p class="lead">STRATEGY</p>
+              <h5>GETTING STARTED</h5>
+            </div>
+          </div>
+        </div>
+        <div class="col-6 col-md-3">
+          <div class="jet-container">
+            <div class="jet-back">
+              <img
+                class="img-fluid"
+                src="./img/jet/pexels-photo-6984550.jpeg"
+                alt=""
+              />
+            </div>
+
+            <div
+              class="
+                overlay
+                jet-front
+                d-flex
+                flex-column
+                align-items-center
+                justify-content-center
+                text-white
+              "
+            >
+              <p class="lead">STRATEGY</p>
+              <h5>GETTING STARTED</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="footer bg-dark text-secondary">
+      <div class="px-3">
+        <div class="row py-5">
+          <div class="col">
+            <p class="mb-3 text-light">LATEST POSTS</p>
+            <div class="mb-3">
+              <a class="nav-link py-0">We moved to our new office</a>
+              <a class="nav-link py-0">March 5, 2021</a>
+            </div>
+            <div class="mb-3">
+              <a class="nav-link py-0">We moved to our new office</a>
+              <a class="nav-link py-0">March 5, 2021</a>
+            </div>
+            <div class="mb-3">
+              <a class="nav-link py-0">We moved to our new office</a>
+              <a class="nav-link py-0">March 5, 2021</a>
+            </div>
+            <div class="mb-3">
+              <a class="nav-link py-0">We moved to our new office</a>
+              <a class="nav-link py-0">March 5, 2021</a>
+            </div>
+          </div>
+          <div class="col">
+            <p class="mb-3 text-light">HEAD OFFICE</p>
+            <p class="px-3 pb-3">
+              59200 Kuala Lumpur, Federal Territory of Kuala Lumpur
+            </p>
+            <div class="mb-3">
+              <a class="nav-link py-0">Karam.99@gmail.com</a>
+              <a class="nav-link py-0">800 1234 56 78</a>
+            </div>
+            <div class="mb-3">
+              <p class="nav-link py-0">Mon-Fri: 9:30 – 21:00</p>
+              <p class="nav-link py-0">at: 10:00 – 15:00</p>
+            </div>
+          </div>
+          <div class="col">
+            <p class="mb-3 text-light">QUICK LINKS</p>
+            <ul class="nav flex-column">
+              <li><a class="nav-link">Home</a></li>
+              <li><a class="nav-link">About us</a></li>
+              <li><a class="nav-link">Services</a></li>
+              <li><a class="nav-link">Projects</a></li>
+              <li><a class="nav-link">Contact</a></li>
+            </ul>
+          </div>
+          <div class="col">
+            <p class="mb-3 text-light">ATTRIBUTIONS</p>
+            <ul>
+              <li>
+                <a href="https://www.freepik.com/vectors/logo"
+                  >Logo vector created by pikisuperstar - www.freepik.com</a
+                >
+              </li>
+            </ul>
+            <p class="mb-3 text-light">NEWSLETTER</p>
+            <div class="input-group">
+              <input
+                class="form-control"
+                type="text"
+                placeholder="Your Email"
+              />
+              <div class="input-group-append">
+                <button class="btn btn-light" type="button">
+                  <i class="fas fa-envelope"></i>
+                </button>
+              </div>
+            </div>
+            <div class="d-flex justify-content-center mt-5">
+              <a href="#"><i class="fa-1x fab fa-linkedin-in"></i></a>
+              <a href="#"><i class="fa-1x ml-4 fab fa-twitter"></i></a>
+              <a href="#"><i class="fa-1x ml-4 fab fa-facebook-f"></i></a>
+              <a href="#"><i class="fa-1x ml-4 fab fa-instagram"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="row px-3 py-3 log-footer justify-content-between">
+          <img src="./invert-logo.png" alt="logo" />
+          <span>
+            &copy; <span id="thisYear">2019</span> Brand. All Rights
+            Reserved</span
+          >
+          <script>
+            const thisDate = new Date();
+            document.getElementById("thisYear").innerHTML =
+              thisDate.getFullYear();
+          </script>
+        </div>
+      </div>
+    </section>
+
+    <!-- scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
+    <script src="./jquery-3.5.1.min.js"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+      integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
+      integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"
+      crossorigin="anonymous"
+    ></script>
+    <script src="./app.js"></script>
+  </body>
+</html>
